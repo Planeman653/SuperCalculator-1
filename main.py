b4d3fd1ee3a22a372840187ad79d4332123bcba5
Dev0verflow <100643134+Dev0verflow@users.noreply.github.com>
2022-05-26 14:45:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from fractions import Fraction
 sym.init_printing()
 
 def inputswitch():
-  print(Fore.YELLOW + "1 for Arithmetic | 2 for Algebra | 3 for Indices | 4 for Fraction, Decimal & Percentage Converter | 5 for help")
+  print(Fore.YELLOW + "1 for Arithmetic | 2 for Algebra | 3 for Indices | 4 for Fraction, Decimal & Percentage Converter | 5 for HCF Calculator | 6 for LCM Calculator | 7 for Prime Number Checker | 8 for help")
   maininput = input(Fore.WHITE + "[>] ")
   if maininput == "1":
     maininput1()
@@ -25,8 +25,14 @@ def inputswitch():
     maininput4()
   elif maininput == "5":
     maininput5()
+  elif maininput == "6":
+    maininput6()
+  elif maininput == "7":
+    maininput7()
+  elif maininput == "8":
+    maininput8()
   else:
-    print("Enter 1,2,3,4 or 5")
+    print("Enter enter a number from 1-8")
     inputswitch()
 def helpinputswitch():
   print("1 For Arithmetic Help | 2 For Algebraic Help | 3 For Indice Help | 4 for Fraction, Decimal And Percentage Converter | 5 For Main Menu")
@@ -157,7 +163,7 @@ def maininput3():
   
   inputswitch()
 
-# Help book
+# Fractions, Decimals and Percentage converter
 
 def maininput4():
   clearConsole()
@@ -236,8 +242,101 @@ def maininput4():
   inputswitch()
 
 def maininput5():
+  clearConsole()
+  print(Fore.RED + """
+██╗░░██╗░█████╗░███████╗  ░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░████████╗░█████╗░██████╗░
+██║░░██║██╔══██╗██╔════╝  ██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+███████║██║░░╚═╝█████╗░░  ██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║░░░██║░░░██║░░██║██████╔╝
+██╔══██║██║░░██╗██╔══╝░░  ██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║░░░██║░░░██║░░██║██╔══██╗
+██║░░██║╚█████╔╝██║░░░░░  ╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+╚═╝░░╚═╝░╚════╝░╚═╝░░░░░  ░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝""")
+  def compute_hcf(x, y):
+
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
+
+  print(Fore.WHITE + "Enter first number.")
+  num1 = int(input(Fore.WHITE + "[>] ")) 
+  print("Enter second number.")
+  num2 = int(input(Fore.WHITE + "[>] "))
+  print("The H.C.F. is", compute_hcf(num1, num2))
+  inputswitch()
+def maininput6():
+  clearConsole()
+  print(Fore.RED + """
+██╗░░░░░░█████╗░███╗░░░███╗  ░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░████████╗░█████╗░██████╗░
+██║░░░░░██╔══██╗████╗░████║  ██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░░░░██║░░╚═╝██╔████╔██║  ██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║░░░██║░░░██║░░██║██████╔╝
+██║░░░░░██║░░██╗██║╚██╔╝██║  ██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║░░░██║░░░██║░░██║██╔══██╗
+███████╗╚█████╔╝██║░╚═╝░██║  ╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+╚══════╝░╚════╝░╚═╝░░░░░╚═╝  ░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝""")
+  def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+    
+  print(Fore.WHITE + "Enter first number.")
+  num1 = int(input(Fore.WHITE + "[>] ")) 
+  print("Enter second number.")
+  num2 = int(input(Fore.WHITE + "[>] "))
+  print("The L.C.M. is", compute_lcm(num1, num2))
+  inputswitch()
+def maininput7():
+  clearConsole()
+  print(Fore.RED + """
+██████╗░██████╗░██╗███╗░░░███╗███████╗  ███╗░░██╗██╗░░░██╗███╗░░░███╗██████╗░███████╗██████╗░
+██╔══██╗██╔══██╗██║████╗░████║██╔════╝  ████╗░██║██║░░░██║████╗░████║██╔══██╗██╔════╝██╔══██╗
+██████╔╝██████╔╝██║██╔████╔██║█████╗░░  ██╔██╗██║██║░░░██║██╔████╔██║██████╦╝█████╗░░██████╔╝
+██╔═══╝░██╔══██╗██║██║╚██╔╝██║██╔══╝░░  ██║╚████║██║░░░██║██║╚██╔╝██║██╔══██╗██╔══╝░░██╔══██╗
+██║░░░░░██║░░██║██║██║░╚═╝░██║███████╗  ██║░╚███║╚██████╔╝██║░╚═╝░██║██████╦╝███████╗██║░░██║
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝
+
+░█████╗░██╗░░██╗███████╗░█████╗░██╗░░██╗███████╗██████╗░
+██╔══██╗██║░░██║██╔════╝██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+██║░░╚═╝███████║█████╗░░██║░░╚═╝█████═╝░█████╗░░██████╔╝
+██║░░██╗██╔══██║██╔══╝░░██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+╚█████╔╝██║░░██║███████╗╚█████╔╝██║░╚██╗███████╗██║░░██║
+░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝""")
+  print(Fore.WHITE + "Enter number.")
+  num = int(input(Fore.WHITE + "[>] "))
+  flag = False
+  if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+  if flag:
+    print(num, "is not a prime number")
+  else:
+    print(num, "is a prime number")
+
+  inputswitch()
+  
+def maininput8():
   
   clearConsole()
+
+# Help book
   
   print(Fore.RED + """
 ██╗░░██╗███████╗██╗░░░░░██████╗░
@@ -280,12 +379,11 @@ def helpmaininput3():
     
     helpinputswitch()
 
-# End of code
-
 def helpmaininput4():
-  print(Fore.WHITE + "Follow all instructions on the converter to use it. Note - Do not use the % symbol")
+  print(Fore.WHITE + "Follow all instructions on the converter to use it. Note - Do not use the % symbol.")
 
   helpinputswitch()
 
 mainmenu()
+
 # End Of Code
